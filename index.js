66f60b9b9f07e719f6987d1b3626c5d57501cf88
Samuel Cifuentes <jsoporte013@gmail.com>
2023-04-28 01:54:53 -0500

    Path * agregado

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
 
-
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html');
+});
 
 
 //Escuchar peticiones
